c61e5fc0f7d013695fb043456ee6c6960ec5e0ab
ALX <alexmonac13@gmail.com>
2023-05-11 23:48:08 +0200

    [ Update ] view_home first version

--- Views/view_home.php
+++ Views/view_home.php
@@ -1,1 +1,6 @@
-<h1>Hello World</h1>
+<main id="home-main">
+    <img src="Content/img/logo_detoure.png" alt="#" style="width: 60%;
+height: 60%;">
+    <h1 id="home-h1-bytemaster">ByteMaster</h1>
+    <button id="home-btn-jouer"><span id="home-span-jouer">Jouer</span></button>
+</main>

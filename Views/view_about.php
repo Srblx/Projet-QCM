7686c452117c46cd00e2edfb2b108ff1e0a111d9
ALX <alexmonac13@gmail.com>
2023-05-15 11:53:48 +0200

    [ Update ] View contact v1

--- Views/view_about.php
+++ Views/view_about.php
@@ -1,5 +1,5 @@
 <main id="about-main">
-    <h1 id="about-h1-faq">FAQ</h1>
+    <h1 id="about-h1">FAQ</h1>
     <div class="about-div-qr">
         <div class="about-question">Question 1</div>
         <div class="about-answer">Réponse 1</div>

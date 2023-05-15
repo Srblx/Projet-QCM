c7b96d5fd795076eae07a3ac709aa8ae89f377a9
ALX <alexmonac13@gmail.com>
2023-05-15 14:27:45 +0200

    [ Update ] Modif view_contact

--- Views/view_contact.php
+++ Views/view_contact.php
@@ -1,15 +1,15 @@
-<main id="contact-main">
-    <form id="contact-form" action="#" method="post">
-        <div id="contact-header-form">
-            <h3 id="contact-h3-form">Contactez-nous</h3>
-            <div id="contact-h3-underline"></div>
+<main id="contact_main">
+    <form id="contact_form" action="#" method="post">
+        <div id="contact_header_form">
+            <h3 id="contact_h3_form">Contactez-nous</h3>
+            <div id="contact_h3_underline"></div>
         </div>
-        <div class="contact-inputs">
-            <input type="text" id="contact-name" name="contact-name" placeholder="Nom">
-            <input type="email" id="contact-email" name="contact-email" placeholder="E-mail">
-            <input type="text" id="contact-object" name="contact-object" placeholder="Objet">
-            <textarea id="contact-message" name="contact-message" rows="8" cols="50" placeholder="Message"></textarea>
-            <input type="submit" id="contact-submit" name="contact-submit" value="Envoyer">
+        <div class="contact_inputs">
+            <input type="text" id="contact_name" name="contact_name" placeholder="Nom">
+            <input type="email" id="contact_email" name="contact_email" placeholder="E_mail">
+            <input type="text" id="contact_object" name="contact_object" placeholder="Objet">
+            <textarea id="contact_message" name="contact_message" rows="8" cols="50" placeholder="Message"></textarea>
+            <input type="submit" id="contact_submit" name="contact_submit" value="Envoyer">
         </div>
     </form>
 </main>

58440193238b70bbcbcfccec867daff959609f88
MSO <mathieu.soussignan@hotmail.fr>
2023-05-15 11:47:14 +0200

    [Improved] Ajout du fichier QuestionModel.php et écriture du code dans Controller.php

--- 2_User/Controllers/Controller_question_correction.php
+++ 2_User/Controllers/Controller_question_correction.php
@@ -1,7 +1,43 @@
 <?php
 
+use QuestionModel;
+
 class Controller_question_correction extends Controller
 {
+    public function action_default()
+    {
+        // Récupérer les réponses de l'utilisateur depuis la session ou une autre source de données
+        $userAnswers = $_SESSION['user_answers']; // Exemple avec une session
 
+        // Charger les questions et les réponses correctes depuis la base de données ou une autre source de données
+        $questionModel = new QuestionModel();
+        $questions = $questionModel->get_all_questions(); // Exemple avec une classe de modèle
 
+        // Vérifier les réponses de l'utilisateur et générer les données de correction
+        $correctionData = $this->generate_correction_data($questions, $userAnswers);
+
+        // Charger la vue de correction et transmettre les données
+        $this->render('question/index', $correctionData);
+    }
+
+    private function generate_correction_data($questions, $userAnswers)
+    {
+        $correctionData = array();
+
+        // Parcourir les questions et comparer les réponses de l'utilisateur avec les réponses correctes
+        foreach ($questions as $index => $question) {
+            $userAnswer = $userAnswers[$index];
+            $isCorrect = $question->correctAnswer($userAnswer);
+
+            // Ajouter les données de correction pour chaque question
+            $correctionData[] = array(
+                'question' => $question->text,
+                'userAnswer' => $userAnswer,
+                'correctAnswer' => $question->answer,
+                'isCorrect' => $isCorrect
+            );
+        }
+
+        return $correctionData;
+    }
 }
--- 2_User/Models/Model.php
+++ 2_User/Models/Model.php
@@ -1,35 +1,33 @@
 <?php
 
 class Model
-{   //* Début de la Classe
-
-    private $bd;
-
+{
+    private $db;
     private static $instance = null;
 
-    /*
-         * Constructeur créant l'objet PDO et l'affectant à $bd
-         */
     private function __construct()
-    {  //* Fonction qui sert à faire le lien avec la BDD
-
-        $dsn = "mysql:host=localhost;dbname=bibliotheque";   //* Coordonnées de la BDD
-        $login = "root";   //* Identifiant d'accès à la BDD
-        $mdp = ""; //* Mot de passe d'accès à la BDD
-        $this->bd = new PDO($dsn, $login, $mdp);
-        $this->bd->query("SET NAMES 'utf8'");
-        $this->bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    {
+        $dsn = "mysql:host=localhost;dbname=qcm";
+        $username = "root";
+        $password = "";
+        $options = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+        );
+        $this->db = new PDO($dsn, $username, $password, $options);
     }
 
-
-    //* get_model()
-
-    public static function get_model()
+    public static function get_instance()
     {
-        //* Fonction qui sert à créer une instance de Model pour l'appeler dans chaque Controller (équivalent de $connex)
         if (is_null(self::$instance)) {
             self::$instance = new Model();
         }
         return self::$instance;
+    }
+
+    public function get_database()
+    {
+        return $this->db;
     }
 }

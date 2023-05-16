aef31d74f60b9dccff066972ccd315459aaeedba
ALX <alexmonac13@gmail.com>
2023-05-16 11:37:38 +0200

    [ Update] Contact send email v1

--- 2_User/Views/view_choice.php
+++ 2_User/Views/view_choice.php
@@ -4,27 +4,27 @@
             <p class="text-title">HTML</p>
             <p class="text-body">Here are the details of the card</p>
         </div>
-        <a href="?controller=choice&action=choice_difficulty"><button class="card-button">Choisir</button></a>
+        <a href="?controller=choice&action=choice_difficulty&id=1"><button class="card-button">Choisir</button></a>
     </div>
     <div class="card">
         <div class="card-details">
             <p class="text-title">CSS</p>
             <p class="text-body">Here are the details of the card</p>
         </div>
-        <a href="?controller=choice&action=choice_difficulty"><button class="card-button">Choisir</button></a>
+        <a href="?controller=choice&action=choice_difficulty&id=2"><button class="card-button">Choisir</button></a>
     </div>
     <div class="card">
         <div class="card-details">
             <p class="text-title">JS</p>
             <p class="text-body">Here are the details of the card</p>
         </div>
-        <a href="?controller=choice&action=choice_difficulty"><button class="card-button">Choisir</button></a>
+        <a href="?controller=choice&action=choice_difficulty&id=3"><button class="card-button">Choisir</button></a>
     </div>
     <div class="card">
         <div class="card-details">
             <p class="text-title">PHP</p>
             <p class="text-body">Here are the details of the card</p>
         </div>
-        <a href="?controller=choice&action=choice_difficulty"><button class="card-button">Choisir</button></a>
+        <a href="?controller=choice&action=choice_difficulty&id=4"><button class="card-button">Choisir</button></a>
     </div>
 </section>
--- Views/view_contact.php
+++ Views/view_contact.php
@@ -1,12 +1,12 @@
 <main id="contact_main">
-    <form id="contact_form" action="#" method="post">
+    <form id="contact_form" action="?controller=contact&action=action_contact_send_mail" method="post">
         <div id="contact_header_form">
             <h3 id="contact_h3_form">Contactez-nous</h3>
             <div id="contact_h3_underline"></div>
         </div>
         <div class="contact_inputs">
             <input type="text" id="contact_name" name="contact_name" placeholder="Nom">
-            <input type="email" id="contact_email" name="contact_email" placeholder="E_mail">
+            <input type="email" id="contact_email" name="contact_email" placeholder="E-mail">
             <input type="text" id="contact_object" name="contact_object" placeholder="Objet">
             <textarea id="contact_message" name="contact_message" rows="8" cols="50" placeholder="Message"></textarea>
             <input type="submit" id="contact_submit" name="contact_submit" value="Envoyer">

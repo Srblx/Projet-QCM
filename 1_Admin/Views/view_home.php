ea42098ec716ac59036aac720554474c73ae601f
SXBL <122864409+Srblx@users.noreply.github.com>
2023-05-16 11:45:01 +0200

    ajout page accueil admin

--- 1_Admin/Views/view_home.php
+++ 1_Admin/Views/view_home.php
@@ -1,1 +1,4 @@
-<h1>Hello World</h1>
+<main id="home-main">
+  <img src="Content/img/logo_detoure.png" alt="#" style="width: 35%; height: 60%;">
+  <button id="home-btn-jouer"><span id="home-span-jouer">Jouer</span></button>
+</main>

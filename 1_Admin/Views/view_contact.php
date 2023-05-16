079938050546c28a967447bdefea15727e411d49
ALX <alexmonac13@gmail.com>
2023-05-16 16:22:41 +0200

    [ Update ] New contact form

---
+++ 1_Admin/Views/view_contact.php
@@ -0,0 +1,16 @@
+<main id="contact_main">
+    <div class="contact-form">
+        <span class="heading">Nous contacter</span>
+        <form action="#" method="post">
+            <label for="nom_contact">Nom:</label>
+            <input type="text" id="nom_contact" name="nom_contact">
+            <label for="prenom_contact">Prenom:</label>
+            <input type="text" id="prenom_contact" name="prenom_contact">
+            <label for="email_contact">Email:</label>
+            <input type="email" id="email_contact" name="email_contact">
+            <label for="message">Message:</label>
+            <textarea id="message_contact" name="message"></textarea>
+            <button type="submit" name="submit_contact">Envoyer</button>
+        </form>
+    </div>
+</main>
--- Views/view_contact.php
+++ Views/view_contact.php
@@ -1,35 +1,16 @@
 <main id="contact_main">
-    <form class="form" action="?controller=contact&action=action_contact_send_mail" method="post">
-        <div class="form-title"><span>Contactez-nous</span></div>
-        <div class="input-container">
-            <input class="input" type="text" id="contact_name" name="contact_name" placeholder="Nom">
-            <span> </span>
-        </div>
-        <div class="input-container">
-            <input class="input" type="email" id="contact_email" name="contact_email" placeholder="E-mail">
-            <span> </span>
-        </div>
-        <div class="input-container">
-            <input class="input" type="text" id="contact_object" name="contact_object" placeholder="Objet">
-            <span> </span>
-        </div>
-        <section class="bg-stars">
-            <span class="star"></span>
-            <span class="star"></span>
-            <span class="star"></span>
-            <span class="star"></span>
-        </section>
-        <div class="input-container">
-            <textarea class="input" id="contact_message" name="contact_message" rows="8" cols="27"
-                placeholder="Message"></textarea>
-            <span> </span>
-        </div>
-        <section class="bg-stars">
-            <span class="star"></span>
-            <span class="star"></span>
-            <span class="star"></span>
-            <span class="star"></span>
-        </section>
-        <input type="submit" class="submit_form_connexion" value="Envoyer" name="contact_submit">
-    </form>
+    <div class="contact-form">
+        <span class="heading">Nous contacter</span>
+        <form action="#" method="post">
+            <label for="nom_contact">Nom:</label>
+            <input type="text" id="nom_contact" name="nom_contact">
+            <label for="prenom_contact">Prenom:</label>
+            <input type="text" id="prenom_contact" name="prenom_contact">
+            <label for="email_contact">Email:</label>
+            <input type="email" id="email_contact" name="email_contact">
+            <label for="message">Message:</label>
+            <textarea id="message_contact" name="message"></textarea>
+            <button type="submit" name="submit_contact">Envoyer</button>
+        </form>
+    </div>
 </main>

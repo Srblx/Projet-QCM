07d2effc15c8d2599b5423b9d87e93400bbfaab0
4hm3dx <madaniahmedlb13@gmail.com>
2023-05-16 11:35:18 +0200

    modif des button des cards de la page choice

--- 2_User/Views/view_choice.php
+++ 2_User/Views/view_choice.php
@@ -2,28 +2,28 @@
     <div class="card">
         <div class="card-details">
             <p class="text-title">HTML</p>
-            <p class="text-body">Here are the details of the card</p>
+            <p class="text-body">La fondation de la structure web</p>
         </div>
         <a href="?controller=choice&action=choice_difficulty"><button class="card-button">Choisir</button></a>
     </div>
     <div class="card">
         <div class="card-details">
             <p class="text-title">CSS</p>
-            <p class="text-body">Here are the details of the card</p>
+            <p class="text-body">L'art de la mise en forme élégante</p>
         </div>
         <a href="?controller=choice&action=choice_difficulty"><button class="card-button">Choisir</button></a>
     </div>
     <div class="card">
         <div class="card-details">
             <p class="text-title">JS</p>
-            <p class="text-body">Here are the details of the card</p>
+            <p class="text-body">La puissance du côté client</p>
         </div>
         <a href="?controller=choice&action=choice_difficulty"><button class="card-button">Choisir</button></a>
     </div>
     <div class="card">
         <div class="card-details">
             <p class="text-title">PHP</p>
-            <p class="text-body">Here are the details of the card</p>
+            <p class="text-body">Le moteur côté serveur dynamique</p>
         </div>
         <a href="?controller=choice&action=choice_difficulty"><button class="card-button">Choisir</button></a>
     </div>

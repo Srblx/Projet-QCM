67af2f4ceba221a4a96673c1f32356b5ce03df20
MSO <mathieu.soussignan@hotmail.fr>
2023-05-17 16:00:32 +0200

    [Update] Modification fichiers style_choice.css, Controller_choice.php et view_choice.php

--- 2_User/Views/view_choice.php
+++ 2_User/Views/view_choice.php
@@ -1,30 +1,32 @@
-<section id="section_choice_theme">
-    <div class="card">
-        <div class="card-details">
-            <p class="text-title">HTML</p>
-            <p class="text-body">La fondation de la structure web</p>
+<main id="main_choice_theme">
+    <section id="section_choice_theme">
+        <div class="card">
+            <div class="card-details">
+                <p class="text-title">HTML</p>
+                <p class="text-body">La fondation de la structure web</p>
+            </div>
+            <a href="?controller=choice&action=choice_difficulty&id=1"><button class="card-button">Choisir</button></a>
         </div>
-        <a href="?controller=choice&action=choice_difficulty&id=1"><button class="card-button">Choisir</button></a>
-    </div>
-    <div class="card">
-        <div class="card-details">
-            <p class="text-title">CSS</p>
-            <p class="text-body">L'art de la mise en forme élégante</p>
+        <div class="card">
+            <div class="card-details">
+                <p class="text-title">CSS</p>
+                <p class="text-body">L'art de la mise en forme élégante</p>
+            </div>
+            <a href="?controller=choice&action=choice_difficulty&id=2"><button class="card-button">Choisir</button></a>
         </div>
-        <a href="?controller=choice&action=choice_difficulty&id=2"><button class="card-button">Choisir</button></a>
-    </div>
-    <div class="card">
-        <div class="card-details">
-            <p class="text-title">JS</p>
-            <p class="text-body">La puissance du côté client</p>
+        <div class="card">
+            <div class="card-details">
+                <p class="text-title">JS</p>
+                <p class="text-body">La puissance du côté client</p>
+            </div>
+            <a href="?controller=choice&action=choice_difficulty&id=3"><button class="card-button">Choisir</button></a>
         </div>
-        <a href="?controller=choice&action=choice_difficulty&id=3"><button class="card-button">Choisir</button></a>
-    </div>
-    <div class="card">
-        <div class="card-details">
-            <p class="text-title">PHP</p>
-            <p class="text-body">Le moteur côté serveur dynamique</p>
+        <div class="card">
+            <div class="card-details">
+                <p class="text-title">PHP</p>
+                <p class="text-body">Le moteur côté serveur dynamique</p>
+            </div>
+            <a href="?controller=choice&action=choice_difficulty&id=4"><button class="card-button">Choisir</button></a>
         </div>
-        <a href="?controller=choice&action=choice_difficulty&id=4"><button class="card-button">Choisir</button></a>
-    </div>
-</section>
+    </section>
+</main>

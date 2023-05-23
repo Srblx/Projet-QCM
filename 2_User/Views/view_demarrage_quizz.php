dd434e07613a280c20f5c9af5783726f7d9f04c6
4hm3dx <madaniahmedlb13@gmail.com>
2023-05-23 12:24:20 +0200

    finalisation page de demarrage quizz, avant optimisation du code

--- 2_User/Views/view_demarrage_quizz.php
+++ 2_User/Views/view_demarrage_quizz.php
@@ -1,5 +1,123 @@
-<main id="case_main_demarrage">
-    <section id="case_section_demarrage">
-        <p>salut alex le boatard</p>
+<main class="case_main_demarrage">
+    <section class="case_section_demarrage">
+        <!-- la condition correspond à ce qu'on trouve dans l'url, et selon l'url, on affiche le contenu qui correspond au thème, ainsi qu'au niveau choisi -->
+        <?php if ($_GET['controller'] === 'choice' && $_GET['action'] === 'demarrage_quizz' && $_GET['id'] === '1' && $_GET['niveau'] === 'debutant'): ?>
+            <h3 class="titre_section_demarrage">HTML</h3>
+            <p>Niveau débutant</p>
+            <img src="./Content/img/html_img.png" alt="" class="img_section_demarrage" id="img_section_demarrage">
+            <p class="para_section_demarrage">Ce QCM comporte 20 questions, pour chaque question vous avez 45s pour
+                répondre.</p>
+            <button type="submit" class="submit_section_demarrage">Démarrer le Quizz</button>
+        </section>
+    </main>
+<?php endif; ?>
+
+
+<?php if ($_GET['controller'] === 'choice' && $_GET['action'] === 'demarrage_quizz' && $_GET['id'] === '2' && $_GET['niveau'] === 'debutant'): ?>
+    <h3 class="titre_section_demarrage">CSS</h3>
+    <p>Niveau débutant</p>
+    <img src="./Content/img/css_img.png" alt="" class="img_section_demarrage" id="img_section_demarrage">
+    <button type="submit" class="submit_section_demarrage">Démarrer le Quizz</button>
     </section>
-</main>
+    </main>
+<?php endif; ?>
+
+
+<?php if ($_GET['controller'] === 'choice' && $_GET['action'] === 'demarrage_quizz' && $_GET['id'] === '3' && $_GET['niveau'] === 'debutant'): ?>
+    <h3 class="titre_section_demarrage">JavaScript</h3>
+    <p>Niveau débutant</p>
+    <img src="" alt="" class="img_section_demarrage" id="img_section_demarrage">
+    <button type="submit" class="submit_section_demarrage">Démarrer le Quizz</button>
+    </section>
+    </main>
+<?php endif; ?>
+
+
+<?php if ($_GET['controller'] === 'choice' && $_GET['action'] === 'demarrage_quizz' && $_GET['id'] === '4' && $_GET['niveau'] === 'debutant'): ?>
+    <h3 class="titre_section_demarrage">PHP</h3>
+    <p>Niveau débutant</p>
+    <img src="" alt="" class="img_section_demarrage" id="img_section_demarrage">
+    <button type="submit" class="submit_section_demarrage">Démarrer le Quizz</button>
+    </section>
+    </main>
+<?php endif; ?>
+
+
+<?php if ($_GET['controller'] === 'choice' && $_GET['action'] === 'demarrage_quizz' && $_GET['id'] === '1' && $_GET['niveau'] === 'intermediaire'): ?>
+    <h3 class="titre_section_demarrage">HTML</h3>
+    <p>Niveau intermédiaire</p>
+    <img src="" alt="" class="img_section_demarrage" id="img_section_demarrage">
+    <button type="submit" class="submit_section_demarrage">Démarrer le Quizz</button>
+    </section>
+    </main>
+<?php endif; ?>
+
+
+<?php if ($_GET['controller'] === 'choice' && $_GET['action'] === 'demarrage_quizz' && $_GET['id'] === '2' && $_GET['niveau'] === 'intermediaire'): ?>
+    <h3 class="titre_section_demarrage">CSS</h3>
+    <p>Niveau intermédiaire</p>
+    <img src="" alt="" class="img_section_demarrage" id="img_section_demarrage">
+    <button type="submit" class="submit_section_demarrage">Démarrer le Quizz</button>
+    </section>
+    </main>
+<?php endif; ?>
+
+
+<?php if ($_GET['controller'] === 'choice' && $_GET['action'] === 'demarrage_quizz' && $_GET['id'] === '3' && $_GET['niveau'] === 'intermediaire'): ?>
+    <h3 class="titre_section_demarrage">JavaScript</h3>
+    <p>Niveau intermédiaire</p>
+    <img src="" alt="" class="img_section_demarrage" id="img_section_demarrage">
+    <button type="submit" class="submit_section_demarrage">Démarrer le Quizz</button>
+    </section>
+    </main>
+<?php endif; ?>
+
+
+<?php if ($_GET['controller'] === 'choice' && $_GET['action'] === 'demarrage_quizz' && $_GET['id'] === '4' && $_GET['niveau'] === 'intermediaire'): ?>
+    <h3 class="titre_section_demarrage">PHP</h3>
+    <p>Niveau intermédiaire</p>
+    <img src="" alt="" class="img_section_demarrage" id="img_section_demarrage">
+    <button type="submit" class="submit_section_demarrage">Démarrer le Quizz</button>
+    </section>
+    </main>
+<?php endif; ?>
+
+
+<?php if ($_GET['controller'] === 'choice' && $_GET['action'] === 'demarrage_quizz' && $_GET['id'] === '1' && $_GET['niveau'] === 'expert'): ?>
+    <h3 class="titre_section_demarrage">HTML</h3>
+    <p>Niveau expert</p>
+    <img src="" alt="" class="img_section_demarrage" id="img_section_demarrage">
+    <button type="submit" class="submit_section_demarrage">Démarrer le Quizz</button>
+    </section>
+    </main>
+<?php endif; ?>
+
+
+<?php if ($_GET['controller'] === 'choice' && $_GET['action'] === 'demarrage_quizz' && $_GET['id'] === '2' && $_GET['niveau'] === 'expert'): ?>
+    <h3 class="titre_section_demarrage">CSS</h3>
+    <p>Niveau expert</p>
+    <img src="" alt="" class="img_section_demarrage" id="img_section_demarrage">
+    <button type="submit" class="submit_section_demarrage">Démarrer le Quizz</button>
+    </section>
+    </main>
+<?php endif; ?>
+
+
+<?php if ($_GET['controller'] === 'choice' && $_GET['action'] === 'demarrage_quizz' && $_GET['id'] === '3' && $_GET['niveau'] === 'expert'): ?>
+    <h3 class="titre_section_demarrage">JavaScript</h3>
+    <p>Niveau expert</p>
+    <img src="" alt="" class="img_section_demarrage" id="img_section_demarrage">
+    <button type="submit" class="submit_section_demarrage">Démarrer le Quizz</button>
+    </section>
+    </main>
+<?php endif; ?>
+
+
+<?php if ($_GET['controller'] === 'choice' && $_GET['action'] === 'demarrage_quizz' && $_GET['id'] === '4' && $_GET['niveau'] === 'expert'): ?>
+    <h3 class="titre_section_demarrage">PHP</h3>
+    <p>Niveau expert</p>
+    <img src="" alt="" class="img_section_demarrage" id="img_section_demarrage">
+    <button type="submit" class="submit_section_demarrage">Démarrer le Quizz</button>
+    </section>
+    </main>
+<?php endif; ?>

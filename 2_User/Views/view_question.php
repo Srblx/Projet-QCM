a175e50dc078e944ec0666ccc4147f7510f4b815
MSO <mathieu.soussignan@hotmail.fr>
2023-05-23 16:52:23 +0200

    [Update] Ajout du style pour les questions

--- 2_User/Views/view_question.php
+++ 2_User/Views/view_question.php
@@ -10,19 +10,16 @@
                         $counter++;
                         echo "<h3 class='titre_section_demarrage'>" . $question->question . "</h3>";
                         echo "<ul>";
-
                         // Autres instructions à l'intérieur de la boucle
-                
                         if ($counter === 1) {
                             break; // Arrête la boucle lorsque le compteur est égal à 1
                         }
                     }
-
                 }
                 ?>
                 <form action="">
                     <?php $cpt = 1 ?>
-                    <?php foreach ($questions as $question): ?>
+                    <?php foreach ($questions as $question) : ?>
                         <label for="qst<?= $cpt ?>"><?= $question->description ?></label>
                         <input type="checkbox" name="qst<?= $cpt ?>" id="qst<?= $cpt ?>"><br />
                         <?php $cpt++ ?>

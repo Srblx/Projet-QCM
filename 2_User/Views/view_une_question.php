886ff9ece69f655b5306176a056fcb2a2c495bb1
ALX <alexmonac13@gmail.com>
2023-05-30 14:02:51 +0200

    [ Update ] Add questions informations + hard timer

--- 2_User/Views/view_une_question.php
+++ 2_User/Views/view_une_question.php
@@ -4,10 +4,17 @@
 // echo "</pre>";
 ?>
 <main id="question_qcm_main">
-    <div class="compteur_question_qcm">
-        <p>Question
-            <?= $_SESSION['cpt'] ?>/20
-        </p>
+    <div class="infos_question_qcm">
+        <div class="compteur_question_qcm">
+            <p>Question
+                <?= $_SESSION['cpt'] ?>/20
+            </p>
+        </div>
+        <div class="timer">
+            <p>
+                0:45s
+            </p>
+        </div>
     </div>
     <div class="container_question">
         <h1 id="byte">Quizz ByteMaster</h1>
@@ -22,7 +29,8 @@
                     <?php $cpt = 1 ?>
                     <?php foreach ($reponses as $reponse): ?>
                         <label for="qst<?= $cpt ?>"><?= substr(htmlspecialchars($reponse->reponse), 3) ?>
-                            <input type="checkbox" name="qst<?= $cpt ?>" id="qst<?= $cpt ?>" value="<?php $reponse->correct ?>">
+                            <input type="checkbox" name="qst<?= $cpt ?>" id="qst<?= $cpt ?>"
+                                value="<?php $reponse->correct ?>">
                         </label>
                         <?php $cpt++ ?>
                     <?php endforeach; ?>
@@ -31,10 +39,10 @@
                     <input type="submit" name="submit_question" value="Valider" id="valider_qcm_question">
                 </div>
             </form>
-            <?php 
+            <?php
             $reponseUtilisateur = $_SESSION["reponseUtilisateur"];
-            echo $reponseUtilisateur;  ?>
-            
+            echo $reponseUtilisateur; ?>
+
         </div>
     </div>
 </main>

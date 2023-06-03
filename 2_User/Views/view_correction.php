5387a19c5dc5c6bbbca10cf7c51a3d60d902cc9b
MSO <mathieu.soussignan@hotmail.fr>
2023-06-03 14:53:18 +0200

    [Update] tentative d'ajout de la correction une fois le quiz terminé

--- 2_User/Views/view_correction.php
+++ 2_User/Views/view_correction.php
@@ -1,54 +1,43 @@
 <main id="question_qcm_main">
     <div class="infos_question_qcm">
         <div class="compteur_question_qcm">
-            <p>Question
-                <?= $_SESSION['cpt']+1?>/20
-            </p>
+            <p>Question <?= $_SESSION['cpt']+1?>/20</p>
         </div>
-       
-
     </div>
     <div class="container_question">
         <h1 id="byte">Quizz ByteMaster</h1>
         <div id="quiz">
             <?php
-            // Afficher la valeur de la question
             echo "<h3 class='titre_section_demarrage'>" . $question->question . "</h3>";
             ?>
 
-            <form id="form_qcm_jouer" method="post" action="?controller=question_correction&action=correction">
+            <form id="form_qcm_jouer" method="post" action="?controller=question_correction&action=question_suivante">
                 <div class="reponses-qcm">
                     <?php $cptReponse = 1 ?>
-                 <?php $ReponseDB ="";?>
-
+                    <?php $ReponseDB = ""; ?>
                     <?php foreach ($reponses as $reponse) : ?>
-                        <label for="qst<?= $cptReponse ?>"><?= substr(htmlspecialchars($reponse->reponse), 3) ?>
-                            <input type="checkbox" name="qst<?= $cptReponse ?>" id="qst<?= $cptReponse ?>" value="<?= $reponse->correct ?>">
-                             <?php $ReponseDB .=$reponse->correct;?>
-                        </label>
+                        <?php
+                        $isChecked = false;
+                        $isCorrect = false;
+                        if (isset($_SESSION['ListeReponseUser'][$_SESSION['cpt']])) {
+                            $reponseUtilisateur = $_SESSION['ListeReponseUser'][$_SESSION['cpt']];
+                            $isChecked = $reponseUtilisateur[$cptReponse - 1] == '1';
+                            $isCorrect = $isChecked && $reponse->correct == '1';
+                        }
+                        ?>
+                        <label for="qst<?= $cptReponse ?>"><?= substr(htmlspecialchars($reponse->reponse), 3) ?></label>
+                        <input type="checkbox" name="qst<?= $cptReponse ?>" id="qst<?= $cptReponse ?>" value="<?= $reponse->correct ?>" disabled <?php if ($isChecked) echo "checked" ?>>
+                        <span class="reponse <?= $isCorrect ? "correct" : "incorrect" ?>">
+                            <?= $reponse->reponse ?>
+                        </span>
                         <?php $cptReponse++ ?>
                     <?php endforeach; ?>
-                    <?php 
-                  
-                  echo '<pre>';
-                //   print_r($_SESSION["liste_id"]);
-                  echo '<br>';
-                  echo 'reponse db : ';
-                  print_r($_SESSION['ListeReponseDB']);
-                  echo '<br>';
-                  echo 'reponse user : ';
-                  print_r($_SESSION['ListeReponseUser']);
-                  echo '</pre>';
-                     ?>
-                    <?php $cpt = $_SESSION['cpt']; ?>
-                    <?php $ListeReponseDB = $_SESSION['ListeReponseDB']; ?>
-                  <?php  $ListeReponseDB[$cpt] = $ReponseDB;
-                    $_SESSION['ListeReponseDB']= $ListeReponseDB; ?>
-
                 </div>
                 <div class="qcm-question-valider">
                     <input type="submit" name="submit_question" value="Suivant" id="valider_qcm_question">
-                //! faire une decrementation des question pour la correction 
+                    <input type="hidden" id="timer-value" name="timer_value" value="">
                 </div>
             </form>
-                    </main>
+        </div>
+    </div>
+</main>
